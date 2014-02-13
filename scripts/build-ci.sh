28a21cf19e8882c1bec5d0450072279ba0723083
Erich Douglass <erich.douglass@gmail.com>
2014-02-13 13:43:00 -0800

    Update repo url in Travis CI build script.

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -7,6 +7,6 @@ echo "Pull request: '${TRAVIS_PULL_REQUEST}' on branch '${TRAVIS_BRANCH}'"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "source" ]; then
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "travis-ci"
-    git remote set-url origin https://${GH_OAUTH_TOKEN}@github.com/erd/robolectric.github.io
+    git remote set-url origin https://${GH_OAUTH_TOKEN}@github.com/robolectric/robolectric.github.io
     bundle exec middleman deploy --build-before
 fi
