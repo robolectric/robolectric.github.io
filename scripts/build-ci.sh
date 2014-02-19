3b18592ffc8aad87d8f895de11eb979d0c26058b
Mike Grafton <mgrafton@pivotallabs.com>
2014-02-19 14:36:45 -0800

    Test commit
    Signed-off-by: Kris Kelly <kkelly@pivotallabs.com>

--- scripts/build-ci.sh
+++ scripts/build-ci.sh
@@ -4,6 +4,7 @@ set -e
 
 #
 # Automatically build and push the docs.
+# Test.
 #
 echo "Pull request: '${TRAVIS_PULL_REQUEST}' on branch '${TRAVIS_BRANCH}'"
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "source" ]; then
